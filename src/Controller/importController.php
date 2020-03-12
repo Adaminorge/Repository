53d7c7be195c699a59bef017686facb4dcbbec52
Adam Pietrzak <adaminorge@gmail.com>
2020-03-12 16:08:52 +0100

    Ny11-chart data

--- src/Controller/importController.php
+++ src/Controller/importController.php
@@ -62,20 +62,69 @@ class importController extends AbstractController
                 echo "Jakas straszna kupa!";
             }
 
-
-
-
-
-
-
-
-
-
       //  return $this->render('/templates/open_file.html.twig');
          return $this->redirectToRoute('show_chart',['id'=>$wykres->getId()]);
 
+    }
 
 
+    /**
+     * @Route(path="/odczytaj/{id}", name="odczytaj")
+     * @param $id
+     * @return
+     */
+    public function odczytaj($id){
+
+        //header('Content-Type: application/json');
+
+        $entityManager=$this->getDoctrine()->getManager();
+        $wynik=$entityManager->getRepository(Data::class)->findBy(array('chart_id'=>$id));
+
+
+        $i=0;
+        foreach ($wynik as $key){
+            if($key->getMenuId()==1){
+                $label=explode(';',$key->getDane());
+            }
+            else {
+                $dataset = explode(';', $key->getDane());
+                $a=array_shift($dataset);
+                $b=$dataset;
+                $dataset2[$i]=[$a,$b];
+
+
+                $i++;
+            }
+        }
+
+        //$data=['label'=>$label,'data'=>$dataset2];
+
+        echo ('<pre>');
+        print_r($dataset2);
+ //-----------------------------------------------------label
+        $labelText="";
+        foreach ($label as $key){
+            $labelText.='"'.$key.'";';
+
+        }
+        $labelText=substr($labelText,0,-1);
+
+        $labelText='labels:['.$labelText.'],';
+
+        echo $labelText;
+        //-------------------------------------------------------data
+
+        foreach ($dataset2 as $key){
+
+
+
+        }
+
+
+
+
+
+        return $this;
 
     }
 }
